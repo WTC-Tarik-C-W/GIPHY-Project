edfaad20d2be1487746fb1c20507177828538a9f
Tarik C-W Work Macbook <tarik.canessawright@wundermanthompson.com>
2022-05-26 10:58:16 +0100

    Readded unignored files

---
+++ script.js
@@ -0,0 +1,129 @@
+'use strict';
+// Giphy API key
+const apiKey = 'DqQ3KqY2wdEvA1q58DwmI3RBDJJIXwyz';
+
+
+// Search terms
+const search = 'search';
+let currentSearch = 'search';
+
+
+// Remembered offset
+const offset = {
+    'random': 0,
+    'finder': 0,
+    'trending': 0,
+};
+
+// HTML Objects
+const navRandom = document.querySelector('#nav__icon--random');
+const navFinder = document.querySelector('#nav__icon--finder');
+const navTrending = document.querySelector('#nav__icon--trending');
+
+const secRandom = document.querySelector('.random');
+const secFinder = document.querySelector('.finder');
+const secTrending = document.querySelector('.trending');
+
+const nextButtons = document.querySelectorAll('.button-next')
+const searchbar = document.querySelector('.finder__searchbar');
+
+
+// Navbar
+const navbar = function(button, section) {
+    button.addEventListener('click', () => {
+        // Button Highlighting
+        navRandom.classList.remove('selected');
+        navFinder.classList.remove('selected');
+        navTrending.classList.remove('selected');
+        button.classList.add('selected')
+
+        // Section Show
+        secRandom.classList.add('hidden-section');
+        secFinder.classList.add('hidden-section');
+        secTrending.classList.add('hidden-section');
+        section.classList.remove('hidden-section');
+    });
+};
+navbar(navRandom, secRandom);
+navbar(navFinder, secFinder);
+navbar(navTrending, secTrending);
+
+
+// Reset Random Giph
+nextButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // Get type
+        const type = button.id.replace('_button-next', '');
+
+        offset[type] += 12; // Increase offset
+        addImgs(type, currentSearch, offset[type]);
+    });
+});
+
+
+// Searchbar auto searcher
+searchbar.addEventListener('input', async () => {
+    // Wait and check if continued typing before fetching
+    try {
+        const newSearch = searchbar.value;
+        setTimeout(() => {
+            if(newSearch == searchbar.value) {
+                currentSearch = newSearch || search; // If searbar empty, go by default
+                addImgs('finder', currentSearch);
+                offset['search'] = 0; // Reset Offset
+            };
+        },
+        500);
+    } catch(err) {
+        console.error(err);
+    }
+});
+
+
+// Giphy API fetch
+const getGif = async function(type, search = '', offset = 0, limit = 12) {
+    // Returns gif or array of gifs
+    try {
+        const res = await fetch(`https://api.giphy.com/v1/gifs/${type}?api_key=${apiKey}&q=${search}&limit=${limit}&offset=${offset}`);
+        const resJson = await res.json();
+        const data = (resJson.data.length) ? resJson.data : [resJson.data];
+        return data;
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+
+// Add image(s) to container
+const addImgs = async function(type, search = '', offset = 0, limit = 12) {
+    // Section specific HTML Objects - Before try/catch so can be used in error
+        const error = document.querySelector(`.${type}__notification--error`);
+        const loading = document.querySelector(`.${type}__notification--loading`);
+        const container = document.querySelector(`.${type}__images`);
+    try {
+        error.classList.add('hidden');
+        loading.classList.remove('hidden');
+        if (type == 'finder') type = 'search'; // Translated to work with fetch request
+        container.innerHTML = ''; // Clear container
+        const data = await getGif(type, search, offset, limit);
+        for (const gif of data) {
+            const image = document.createElement('img');
+            image.src = `https://media4.giphy.com/media/${gif.id}/giphy.gif`; // Image address
+            image.alt = gif.title ?? 'Giphy gif';
+            container.append(image); // Add to container
+        };
+        loading.classList.add('hidden');
+    } catch(err) {
+        console.error(err);
+        loading.classList.add('hidden');
+        error.classList.remove('hidden');
+    }
+};
+
+
+// On first load images
+addImgs('random');
+addImgs('finder', search);
+addImgs('trending');
+
+
